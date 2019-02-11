a361239c41dad57c72bec6342108f1ba589e0a78
AlifiahPutri <lifi.alifia9@gmail.com>
2019-02-11 22:51:34 +0700

    Menghitung gaji pegawai (polimorphism)

---
+++ src/com/company/Staff.java
@@ -0,0 +1,10 @@
+package com.company;
+
+public class Staff extends Pegawai {
+    public Staff(){
+        this.Tarif = 50000;
+    }
+    public void data(String id, String nama){
+        System.out.println("Pegawai dengan nomor id " + id + " bernama " + nama);
+    }
+}
---
+++ src/com/company/Supervisor.java
@@ -0,0 +1,10 @@
+package com.company;
+
+public class Supervisor extends Pegawai {
+    public Supervisor(){
+        this.Tarif = 100000;
+    }
+    public void data(String id, String nama){
+        System.out.println("Pegawai dengan nomor id " + id + " bernama " + nama);
+    }
+}
